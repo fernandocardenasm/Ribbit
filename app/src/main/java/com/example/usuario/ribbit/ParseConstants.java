bc17c9dfc652198d2d295cdb26a666139079b74a
Fernando <fernando.cardenasm@gmail.com>
2015-03-19 14:35:11 -0500

    Add Parse constants class

---
+++ app/src/main/java/com/example/usuario/ribbit/ParseConstants.java
@@ -0,0 +1,12 @@
+package com.example.usuario.ribbit;
+
+/**
+ * Created by usuario on 19/03/2015.
+ */
+public final class ParseConstants {
+    //Class Name
+
+    //Field Names
+
+    public static final String KEY_USERNAME = "username";
+}
