f30c79a32adf2afce2ec6888d6e3c18980805c97
Fernando <fernando.cardenasm@gmail.com>
2015-03-20 22:06:18 -0500

    Add AlertDialog  for camera choices

--- app/src/main/java/com/example/usuario/ribbit/MainActivity.java
+++ app/src/main/java/com/example/usuario/ribbit/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.usuario.ribbit;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -16,6 +18,26 @@ import com.parse.ParseUser;
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+
+
+    //Set which action will be triggered depending on which option is clicked
+    private DialogInterface.OnClickListener mDialogListener =
+            new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case 0: //Take picture Option
+
+                    break;
+                case 1: //Take video
+                    break;
+                case 2: //Choose Picture
+                    break;
+                case 3: //Choose Video
+                    break;
+            }
+        }
+    };
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -110,13 +132,21 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_logout) {
-            ParseUser.logOut();
-            navigateToLogin();
-        }
-        else if (id == R.id.action_edit_friends){
-            Intent intent = new Intent(this, EditFriendsActivity.class);
-            startActivity(intent);
+        switch (id){
+            case R.id.action_logout:
+                ParseUser.logOut();
+                navigateToLogin();
+                break;
+            case R.id.action_edit_friends:
+                Intent intent = new Intent(this, EditFriendsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.action_camera:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(R.array.camera_choices, mDialogListener);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
         }
 
         return super.onOptionsItemSelected(item);
