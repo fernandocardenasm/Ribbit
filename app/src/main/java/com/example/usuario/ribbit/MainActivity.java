f49230f6ebcaa2a795959330d464df93efafbdd3
Fernando <fernando.cardenasm@gmail.com>
2015-03-20 22:33:26 -0500

    Add Camera Intent

--- app/src/main/java/com/example/usuario/ribbit/MainActivity.java
+++ app/src/main/java/com/example/usuario/ribbit/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
@@ -19,6 +20,12 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int TAKE_VIDEO_REQUEST = 1;
+    public static final int PICK_PHOTO_REQUEST = 2;
+    public static final int PICK_VIDEO_REQUEST = 3;
+
+
 
     //Set which action will be triggered depending on which option is clicked
     private DialogInterface.OnClickListener mDialogListener =
@@ -27,7 +34,8 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         public void onClick(DialogInterface dialog, int which) {
             switch (which){
                 case 0: //Take picture Option
-
+                    Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST );
                     break;
                 case 1: //Take video
                     break;
