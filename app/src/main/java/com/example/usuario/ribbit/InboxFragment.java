552634cc899110d1ce36b224c645a1316bd4e8e3
Fernando <fernando.cardenasm@gmail.com>
2015-03-23 18:29:18 -0500

    Add Intent to show the video using a preferred device´s video player

--- app/src/main/java/com/example/usuario/ribbit/InboxFragment.java
+++ app/src/main/java/com/example/usuario/ribbit/InboxFragment.java
@@ -88,6 +88,9 @@ public class InboxFragment extends ListFragment {
         }
         else{
             //View the video
+            Intent intent = new Intent(Intent.ACTION_VIEW, fileUri);
+            intent.setDataAndType(fileUri, "video/*");
+            startActivity(intent);
         }
     }
 }
