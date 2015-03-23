1559c9afc8bab4bb9ca1b874090ebc5ffebc8b05
Fernando <fernando.cardenasm@gmail.com>
2015-03-23 18:15:19 -0500

    Add Picasso library to show the selected image

--- app/src/main/java/com/example/usuario/ribbit/ViewImageActivity.java
+++ app/src/main/java/com/example/usuario/ribbit/ViewImageActivity.java
@@ -3,9 +3,9 @@ package com.example.usuario.ribbit;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 
 public class ViewImageActivity extends ActionBarActivity {
@@ -18,28 +18,10 @@ public class ViewImageActivity extends ActionBarActivity {
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
         Uri imageUri = getIntent().getData();
+
+        //Use Picasso to see our image
+
+        Picasso.with(this).load(imageUri.toString()).into(imageView);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_view_image, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
