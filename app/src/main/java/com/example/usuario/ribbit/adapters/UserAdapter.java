f6c98037f67e69deae987ebf1262b04b5f09477e
Fernando <fernando.cardenasm@gmail.com>
2015-04-08 17:40:01 +0200

    Set the checkmarks for the User Grid View

--- app/src/main/java/com/example/usuario/ribbit/adapters/UserAdapter.java
+++ app/src/main/java/com/example/usuario/ribbit/adapters/UserAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -37,6 +38,7 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
             convertView = LayoutInflater.from(mContext).inflate(R.layout.user_item, null);
             holder = new ViewHolder();
             holder.userImageView = (ImageView) convertView.findViewById(R.id.userImageView);
+            holder.checkImageView = (ImageView) convertView.findViewById(R.id.checkImageView);
             holder.nameLabel = (TextView) convertView.findViewById(R.id.nameLabel);
             convertView.setTag(holder);
         }
@@ -68,11 +70,22 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
                     .into(holder.userImageView);
         }
 
+        //We want to know if certain element is checked or not
+        GridView gridView = (GridView) parent;
+
+        if (gridView.isItemChecked(position)){
+            holder.checkImageView.setVisibility(View.VISIBLE);
+        }
+        else{
+            holder.checkImageView.setVisibility(View.INVISIBLE);
+        }
+
         return convertView;
     }
 
     private static class ViewHolder{
         ImageView userImageView;
+        ImageView checkImageView;
         TextView nameLabel;
     }
 
